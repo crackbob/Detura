ee076cd97aeea49713aa204232c66bcabfeacf0c
crackbob <crackbob.999@gmail.com>
2025-02-27 00:30:10 -0500

    i think

--- src/utils/CUtils.js
+++ src/utils/CUtils.js
@@ -1,32 +1,30 @@
-const typeSizes = {
-    'float': 4,
-    'int': 4,
-    'bool': 4,
-    'string': 4,
-    'double': 8,
-    'char': 1,
-    'byte': 1,
-    'short': 2,
-    "uint32_t": 4,
-    'pad_4': 4,
-    'pad_2': 2,
-    "pad_1016": 1016
+let typeMap = {
+    'float': { size: 4, heap: Module.HEAPF32 },
+    'int': { size: 4, heap: Module.HEAP32 },
+    'bool': { size: 4, heap: Module.HEAP32 },
+    'string': { size: 4, heap: Module.HEAP32 },
+    'double': { size: 8, heap: Module.HEAPF64 },
+    'char': { size: 1, heap: Module.HEAP8 },
+    'byte': { size: 1, heap: Module.HEAP8 },
+    'short': { size: 2, heap: Module.HEAP16 },
+    'uint32_t': { size: 4, heap: Module.HEAP32 }
 };
 
 function getOffset(obj, key) {
     let offset = 0;
 
-    for (const k in obj) {
+    for (let k in obj) {
         if (k === key) {
             return offset;
         }
 
-        const type = obj[k];
-
+        let type = obj[k];
         if (typeof type === 'object' && type !== null) {
             offset += getTotalSize(type);
-        } else if (typeSizes[type]) {
-            offset += typeSizes[type];
+        } else if (typeMap[type]) {
+            offset += typeMap[type].size;
+        } else if (type?.includes && type.includes("pad_")) {
+            offset += parseInt(type.replace("pad_", ""));
         }
     }
 
@@ -35,83 +33,53 @@ function getOffset(obj, key) {
 
 function getTotalSize(obj) {
     let totalSize = 0;
-    for (const k in obj) {
-        const type = obj[k];
+
+    for (let k in obj) {
+        let type = obj[k];
 
         if (typeof type === 'object' && type !== null) {
             totalSize += getTotalSize(type);
-        } else if (typeSizes[type]) {
-            totalSize += typeSizes[type];
+        } else if (typeMap[type]) {
+            totalSize += typeMap[type].size;
         }
     }
+
     return totalSize;
 }
 
 function getCValue(type, ptr) {
-    if (type == "float") {
-        return Module.HEAPF32[ptr >> 2];
-    }
-
-    if (type == "int") {
-        return Module.HEAP32[ptr >> 2];
-    }
-
-    if (type == "bool" || type == "short") {
-        return Module.HEAP16[ptr >> 1];
-    }
-
-    if (type === "byte") {
-        return Module.HEAP8[ptr];
-    }
-
-    if (type === "double") {
-        return Module.HEAPF64[ptr >> 3];
-    }
+    let typeInfo = typeMap[type] || typeMap['int'];
+    let index = ptr / typeInfo.size;
+    return typeInfo.heap[index];
 }
 
 function setCValue(type, ptr, value) {
-    if (type == "float") {
-        Module.HEAPF32[ptr >> 2] = value;
-    }
-
-    if (type == "int") {
-        Module.HEAP32[ptr >> 2] = value;
-    }
-
-    if (type == "bool" || type == "short") {
-        Module.HEAP16[ptr >> 1] = value;
-    }
-
-    if (type === "byte") {
-        Module.HEAP8[ptr] = value;
-    }
-
-    if (type === "double") {
-        Module.HEAPF64[ptr >> 3] = value;
-    }
+    let typeInfo = typeMap[type] || typeMap['int'];
+    let index = ptr / typeInfo.size;
+    typeInfo.heap[index] = value;
 }
 
 function cStruct(struct, ptr) {
-    const structObject = {};
+    let structObject = {};
 
-    for (const key in struct) {
-        const type = struct[key];
-        const offset = getOffset(struct, key);
+    for (let key in struct) {
+        let type = struct[key];
+        let offset = getOffset(struct, key);
 
         Object.defineProperty(structObject, key, {
             get() {
                 if (typeof type === 'object' && type !== null) {
                     return cStruct(type, ptr + offset);
-                } else if (typeSizes[type]) {
+                } else if (typeMap[type]) {
                     return getCValue(type, ptr + offset);
                 }
                 return undefined;
             },
             set(value) {
                 if (typeof type === 'object' && type !== null) {
-                    const nestedStruct = cStruct(type, ptr + offset);
+                    let nestedStruct = cStruct(type, ptr + offset);
                     Object.assign(nestedStruct, value);
-                } else if (typeSizes[type]) {
+                } else if (typeMap[type]) {
                     setCValue(type, ptr + offset, value);
                 }
             },
@@ -123,4 +91,4 @@ function cStruct(struct, ptr) {
     return structObject;
 }
 
-export default { typeSizes, getOffset, getTotalSize, getCValue, setCValue, cStruct };
+export default { typeMap, getOffset, getTotalSize, getCValue, setCValue, cStruct };
